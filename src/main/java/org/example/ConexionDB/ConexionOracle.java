7ff7054ffbac7f9a5e599f7a1d4b643417932657
Lamsu1505 <131729688+Lamsu1505@users.noreply.github.com>
2025-04-29 14:45:24 -0500

    Se realizó una prueba de conexion con la base de datos de prueba (Funciona)

---
+++ src/main/java/org/example/ConexionDB/ConexionOracle.java
@@ -0,0 +1,36 @@
+package org.example.ConexionDB;
+
+import java.sql.*;
+
+public class ConexionOracle {
+    private static final String URL = "jdbc:oracle:thin:@localhost:1522:XE";
+    private static final String USUARIO = "SYSTEM";
+    private static final String CONTRASENA = "oracle";
+
+    public static Connection conectar() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection conexion = DriverManager.getConnection(URL, USUARIO, CONTRASENA);
+            System.out.println("Conexión exitosa");
+            return conexion;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
