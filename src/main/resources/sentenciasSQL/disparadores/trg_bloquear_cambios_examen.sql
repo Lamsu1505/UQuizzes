34e61afd3d646d7868f4ac67e34c8512d56c1cb0
AndresZunigaZ2005 <andresf.zunigaz@uqvirtual.edu.co>
2025-05-16 10:04:53 -0500

    crear SQL de validarPreguntas y bloquearCambios del examen ya iniciado

---
+++ src/main/resources/sentenciasSQL/disparadores/trg_bloquear_cambios_examen.sql
@@ -0,0 +1,16 @@
+create or replace TRIGGER trg_bloquear_cambios_examen
+BEFORE UPDATE ON EXAMEN
+FOR EACH ROW
+DECLARE
+v_existe NUMBER;
+BEGIN
+SELECT COUNT(*) INTO v_existe
+FROM SOLUCIONEXAMENESTUDIANTE
+WHERE Examen_idExamen = :OLD.idExamen;
+
+
+IF v_existe > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'No se puede modificar o eliminar un examen que ya fue presentado por al menos un estudiante.');
+END IF;
+END;
+/
