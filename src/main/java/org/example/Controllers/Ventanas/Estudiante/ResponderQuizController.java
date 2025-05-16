d0eb523879728edfb583ccf43c2620d064c3b948
royer111 <royergarci10@gmail.com>
2025-05-16 10:10:29 -0500

    Carga preguntas para responder. Comenzare a cambiar el diseño

--- src/main/java/org/example/Controllers/Ventanas/Estudiante/ResponderQuizController.java
+++ src/main/java/org/example/Controllers/Ventanas/Estudiante/ResponderQuizController.java
@@ -11,22 +11,85 @@ public class ResponderQuizController {
 
     @FXML
     private VBox contenedorPreguntas;
-    public void cargarPreguntaSeleccionada() {
-        try {
-            String fxmlRuta = "/Interfaces/Paneles.Estudiante/FormatoRespuestasQuiz/FormatoMultipleRespuesta.fxml";
-            System.out.println("Cargando FXML: " + fxmlRuta);
 
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlRuta));
-            Node preguntaPane = loader.load();
 
-            contenedorPreguntas.getChildren().add(preguntaPane);
-            System.out.println("Pregunta cargada y añadida");
+    public void verificarTipoPregunta (int tipoPregunta){
+        // Tipo 1: multiple respuesta
+        if(tipoPregunta == 1){
+            try {
+                String fxmlRuta = "/Interfaces/Paneles.Estudiante/FormatoRespuestasQuiz/FormatoMultipleRespuesta.fxml";
+                System.out.println("Cargando FXML: " + fxmlRuta);
 
-        } catch (Exception e) {
-            System.err.println("Error al cargar pregunta: " + e.getMessage());
-            e.printStackTrace();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlRuta));
+                Node preguntaPane = loader.load();
+
+                contenedorPreguntas.getChildren().add(preguntaPane);
+                System.out.println("Pregunta cargada y añadida");
+
+            } catch (Exception e) {
+                System.err.println("Error al cargar pregunta: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+            // Tipo 2: unica respuesta
+        } else if (tipoPregunta ==2) {
+            try {
+                String fxmlRuta = "/Interfaces/Paneles.Estudiante/FormatoRespuestasQuiz/FormatoUnicaRespuesta.fxml";
+                System.out.println("Cargando FXML: " + fxmlRuta);
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlRuta));
+                Node preguntaPane = loader.load();
+
+                contenedorPreguntas.getChildren().add(preguntaPane);
+                System.out.println("Pregunta cargada y añadida");
+
+            } catch (Exception e) {
+                System.err.println("Error al cargar pregunta: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+
+        //Tipo 3: verdader/falso
+        else if (tipoPregunta == 3 ) {
+            try {
+                String fxmlRuta = "/Interfaces/Paneles.Estudiante/FormatoRespuestasQuiz/FormatoVerdaderoFalso.fxml";
+                System.out.println("Cargando FXML: " + fxmlRuta);
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlRuta));
+                Node preguntaPane = loader.load();
+
+                contenedorPreguntas.getChildren().add(preguntaPane);
+                System.out.println("Pregunta cargada y añadida");
+
+            } catch (Exception e) {
+                System.err.println("Error al cargar pregunta: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+
+
+            //Tipo 4: respuesta corta
+        } else if (tipoPregunta ==4) {
+            try {
+                String fxmlRuta = "/Interfaces/Paneles.Estudiante/FormatoRespuestasQuiz/FormatoRespuestaCorta.fxml";
+                System.out.println("Cargando FXML: " + fxmlRuta);
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlRuta));
+                Node preguntaPane = loader.load();
+
+                contenedorPreguntas.getChildren().add(preguntaPane);
+                System.out.println("Pregunta cargada y añadida");
+
+            } catch (Exception e) {
+                System.err.println("Error al cargar pregunta: " + e.getMessage());
+                e.printStackTrace();
+            }
+
         }
     }
+
+
     @FXML
     private Button enviarButton;
 
@@ -36,8 +99,9 @@ public class ResponderQuizController {
     @FXML
     public void initialize() {
         // Aquí llamas a tu método para cargar la interfaz de la pregunta
-        for (int i = 0; i < 3; i++) {
-            cargarPreguntaSeleccionada();
-        }
+       verificarTipoPregunta(1);
+       verificarTipoPregunta(2);
+       verificarTipoPregunta(3);
+       verificarTipoPregunta(4);
     }
 }
